12f4bd69b1b22ddf5936c092762d098e772a9bc4
Karish <danifayn@gmail.com>
2018-03-12 12:27:55 +0200

    kmeans temp fix

--- Implementation/ClassicMethods/KMeans.py
+++ Implementation/ClassicMethods/KMeans.py
@@ -20,6 +20,10 @@ class KMeans(Discretization):
             cutpoints[property_id] = sorted([centroid[0] for centroid in kmeans.cluster_centers_])
         self.bins_cutpoints = cutpoints
 
+    def set_bin_ranges_for_property(self, property_to_entities: Dict[int, Set[Entity]], class_to_entities: Dict[int, Set[Entity]],
+                       property_to_timestamps: Dict[int, List[TimeStamp]], property_id: int):
+        pass
+
     def __init__(self, bin_count):
         super(KMeans, self).__init__()
         self.bin_count = bin_count
