1bccbbd25e9ab3e2dbe52a39e3c3c2a76b95b735
Karish <danifayn@gmail.com>
2018-03-20 14:48:00 +0200

    output handling init

--- Implementation/OutputHandling/Discretization_Out_Handler.py
+++ Implementation/OutputHandling/Discretization_Out_Handler.py
@@ -3,3 +3,15 @@ import Implementation.AbstractDiscretisation as ad
 def convert_cutpoints_to_output(property_to_entities, class_to_entities, property_to_timestamps, cutpoints):
     cutpoints_dict = {}
     cutpoints_df = pd.DataFrame()
+    number_of_entities = 0
+    for _class in class_to_entities:
+        number_of_entities += len(class_to_entities[_class])
+
+    for _class in class_to_entities:
+        # Create name for the _class output file
+        path = r'~/method_Out_Class' + str(_class) + '.txt'
+        with open(path) as f:
+            f.write('startToncepts \n')
+            f.write('numberOfEntities, ' + number_of_entities + '\n')
+            for _entity in class_to_entities[_class]:
+
