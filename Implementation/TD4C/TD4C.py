c40121730b7e33e22c55b1e489debac66c1b9643
Daniel Rejabek <danielrbk@gmail.com>
2018-03-20 16:42:15 +0200

    Added test suite and datasets to git

--- Implementation/TD4C/TD4C.py
+++ Implementation/TD4C/TD4C.py
@@ -14,15 +14,17 @@ import numpy as np
 
 class TD4C(Discretization):
 
-    def __init__(self, bin_count, distance_measure):
+    def __init__(self, bin_count, distance_measure, ACCURACY_MEASURE = 100):
         super(TD4C, self).__init__()
+        self.ACCURACY_MEASURE = ACCURACY_MEASURE
         self.distance_measure = distance_measure
         self.chosen_scores = {}
         self.bin_count = bin_count
         self.candidate_cutpoints = {}
+        self.cutoffs_according_to_order = {}
 
     def set_bin_ranges(self, property_to_entities: Dict[int, Set[Entity]], class_to_entities: Dict[int, Set[Entity]], property_to_timestamps: Dict[int, List[TimeStamp]]):
-        equal_frequency = EqualFrequency(100)
+        equal_frequency = EqualFrequency(self.ACCURACY_MEASURE)
         m1,m2,m3 = equal_frequency.discretize(property_to_entities, class_to_entities, property_to_timestamps)
         self.candidate_cutpoints = equal_frequency.bins_cutpoints
         cutpoints = {}
@@ -35,24 +37,19 @@ class TD4C(Discretization):
                                     class_to_entities: Dict[int, Set[Entity]],
                                     property_to_timestamps: Dict[int, List[TimeStamp]], property_id: int):
         candidate_cutoffs: List[float] = sorted(self.candidate_cutpoints[property_id])
-        print("%s: %s" % (property_id, candidate_cutoffs))
         chosen_cutoffs = SortedList()
         chosen_cutoffs_indices = SortedList()
         cutoffs_according_to_order = []
 
-        class_to_state_vector = {}
+        class_to_state_vector = TD4C.populate_state_vector(property_to_entities, class_to_entities, property_to_timestamps, len(candidate_cutoffs), property_id)
 
-        for c in class_to_entities.keys():
-            class_to_state_vector[c] = [0]*(len(candidate_cutoffs)+1)
-            ctrs = [Counter([int(ts.value) for ts in entity.properties[property_id]]) for entity in
-                    class_to_entities[c] if entity in property_to_entities[property_id]]
-            ctr = sum(ctrs, Counter())
-            for state in ctr.keys():
-                class_to_state_vector[c][state] += ctr[state]
+
 
         chosen_scores = []
-
+        iterations_scores_and_cutoffs = []
+        print("\n---------------------%s----------------------" % property_id)
         for i in range(self.bin_count - 1):
+            scores_and_cutoffs = []
             max_distance = float('-inf')
             best_cutoff = float('-inf')
             best_index = float('-inf')
@@ -64,16 +61,20 @@ class TD4C(Discretization):
                 temp_cutoff_indices.add(j)
                 probability_vector = self.calculate_probability_vector(class_to_state_vector, temp_cutoff_indices)
                 distance_of_series = self.distance_measure(probability_vector)
+                scores_and_cutoffs.append((cutoff, distance_of_series))
                 if distance_of_series > max_distance:
                     max_distance = distance_of_series
                     best_cutoff = cutoff
                     best_index = j
+            print("%s: %s" % (best_cutoff, scores_and_cutoffs))
+            iterations_scores_and_cutoffs.append(scores_and_cutoffs)
             chosen_cutoffs.add(best_cutoff)
             chosen_cutoffs_indices.add(best_index)
             cutoffs_according_to_order.append(best_cutoff)
             chosen_scores.append(max_distance)
 
-        self.chosen_scores.update({property_id:chosen_scores})
+        self.cutoffs_according_to_order.update({property_id: cutoffs_according_to_order})
+        self.chosen_scores.update({property_id: chosen_scores})
         return list(chosen_cutoffs)
 
     def write_auxiliary_information(self, property_to_entities: Dict[int, Set[Entity]],
@@ -178,6 +179,19 @@ class TD4C(Discretization):
             for j in range(i + 1, len(classes)):
                 d += TD4C.__SKL__(class_to_probability_vector[classes[i]],class_to_probability_vector[classes[j]])
         return d
+
+    @staticmethod
+    def populate_state_vector(property_to_entities, class_to_entities, property_to_time_stamps, number_of_cutoffs: int, property_id):
+        class_to_state_vector = {}
+        for c in class_to_entities.keys():
+            class_to_state_vector[c] = [0]*(number_of_cutoffs+1)
+            ctrs = [Counter([int(ts.value) for ts in entity.properties[property_id]]) for entity in
+                    class_to_entities[c] if entity in property_to_entities[property_id]]
+            ctr = sum(ctrs, Counter())
+            for state in ctr.keys():
+                class_to_state_vector[c][state] += ctr[state]
+        return class_to_state_vector
+
 
 
 
